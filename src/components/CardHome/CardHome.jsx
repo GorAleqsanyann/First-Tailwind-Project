1acccb021cec099f530d0b5d6451ec7ee7c4703e
GorAleqsanyann <g.aleqsanyan00@mail.ru>
2025-02-16 12:58:30 +0400

    home-update

---
+++ src/components/CardHome/CardHome.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function CardHome({ image, text, wiki }) {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="flex flex-col gap-1 text-center">
+      <img
+        className="w-[300px] h-[300px]"
+        src={image}
+        onClick={() => setShow((e) => !e)}
+      />
+      {show && (
+        <label>
+          <a href={wiki}>{text}</a>
+        </label>
+      )}
+    </div>
+  );
+}
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,3 +1,37 @@
+import CardHome from "../../components/CardHome/CardHome";
+
+import img1 from "./images/ron1.jpg";
+import img2 from "./images/pele1.jpg";
+import img3 from "./images/messi1.jpg";
+
 export default function Home() {
-  return <div>Home</div>;
+  const images = [
+    {
+      id: 1,
+      image: img1,
+      text: "haytni xaxacox Ronaldo",
+      wiki: "https://en.wikipedia.org/wiki/Cristiano_Ronaldo",
+    },
+    {
+      id: 2,
+      image: img2,
+      text: "haytni xaxacox Pele",
+      wiki: "https://ru.wikipedia.org/wiki/Пеле",
+    },
+    {
+      id: 3,
+      image: img3,
+      text: "haytni xaxacox Messi",
+      wiki: "https://en.wikipedia.org/wiki/Lionel_Messi",
+    },
+  ];
+  return (
+    <div className="flex justify-around items-center p-5">
+      {images.map((e) => (
+        <div key={e.id}>
+          <CardHome image={e.image} text={e.text} wiki={e.wiki} />
+        </div>
+      ))}
+    </div>
+  );
 }
